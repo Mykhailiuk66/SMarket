1de3fdadbd71200ff7f2e287d5d8ce93e7ea52d0
Vitaliy <mykhaylyuk66@gmail.com>
2022-06-09 15:35:17 +0300

    Bug fixes

--- trade_hub/admin.py
+++ trade_hub/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+
+admin.site.register(models.Item)
+admin.site.register(models.UserItem)
+admin.site.register(models.Trade)
+admin.site.register(models.TradeItem)
+admin.site.register(models.ItemQuality)
+admin.site.register(models.ItemExterior)
+admin.site.register(models.Game)
+
--- trade_hub/models.py
+++ trade_hub/models.py
@@ -17,7 +17,7 @@ class Item(models.Model):
 
 
     def __str__(self):
-        return "StatTrak™" if self.stattrak else "" + f"{self.name} ({self.quality.name})"
+        return ("StatTrak™ " if self.stattrak else "") + f"{self.name} ({self.quality.name})"
     
     class Meta:
         ordering = ["name"]
@@ -26,8 +26,8 @@ class Item(models.Model):
 
 class UserItem(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
-    price = models.DecimalField(decimal_places=2)
-    float = models.DecimalField(max_digits=18, ecimal_places=17, blank=True, null=True)
+    price = models.DecimalField(max_digits=9, decimal_places=2)
+    float = models.DecimalField(max_digits=18, decimal_places=17, blank=True, null=True)
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
     user = models.ForeignKey(Profile, on_delete=models.CASCADE)
 
@@ -49,9 +49,9 @@ class Trade(models.Model):
 
 
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
-    creator = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True)
-    second_party = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True)
-    quarantor = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True)
+    creator = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True, related_name="creator")
+    second_party = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True, related_name="second_party")
+    guarantor = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True, related_name="guarantor")
     status = models.CharField(max_length=2, choices=STATUSES, default=NEW)
     created = models.DateTimeField(auto_now_add=True)
 
@@ -81,7 +81,7 @@ class ItemQuality(models.Model):
     game = models.ForeignKey("trade_hub.Game", on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.name}({self.game.short_name})"
+        return f"{self.name} ({self.game.short_name})"
 
 
 class ItemExterior(models.Model):
@@ -90,7 +90,7 @@ class ItemExterior(models.Model):
     game = models.ForeignKey("trade_hub.Game", on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.name}({self.game.short_name})"
+        return f"{self.name} ({self.game.short_name})"
 
 
 class Game(models.Model):
@@ -99,4 +99,4 @@ class Game(models.Model):
     short_name = models.CharField(max_length=50)
 
     def __str__(self):
-        return f"{self.name}({self.short_name})"
+        return f"{self.name} ({self.short_name})"
