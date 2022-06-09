99b0cf76717d6203def5c1084af74d6fc8d3b75c
Vitaliy <mykhaylyuk66@gmail.com>
2022-06-09 10:45:08 +0300

    Add basic templates

--- smarket/settings.py
+++ smarket/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'trade_hub',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,9 @@ ROOT_URLCONF = 'smarket.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
---
+++ trade_hub/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TradeHubConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'trade_hub'
