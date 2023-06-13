ee14abcac175d307c6e40dbb03c6a52253f80e89
Vitaliy <mykhaylyuk66@gmail.com>
2023-06-13 15:34:23 +0300

    Add 'generate_random_inventory' method for testing purposes

---
+++ users/utils.py
@@ -0,0 +1,11 @@
+from trade_hub.models import Item, Trade, UserItem
+from django.db.models import Q
+
+
+def generate_random_inventory(profile):
+    profile.creator.filter(Q(status=Trade.NEW) | Q(status=Trade.REVIEWING)).delete()
+    profile.useritem_set.all().update(user=None)
+
+    items = Item.objects.order_by('?')[:5]
+
+    [UserItem.objects.create(item=item, user=profile) for item in items]
