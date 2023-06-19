7b8e05b4fbb20eeba96326380591fa0cc1f79723
Vitalii <110096259+Mykhailiuk66@users.noreply.github.com>
2023-06-19 10:53:41 +0200

    Minor improvements

--- users/utils.py
+++ users/utils.py
@@ -6,6 +6,6 @@ def generate_random_inventory(profile):
     profile.creator.filter(Q(status=Trade.NEW) | Q(status=Trade.REVIEWING)).delete()
     profile.useritem_set.all().update(user=None)
 
-    items = Item.objects.order_by('?')[:5]
+    items = Item.objects.order_by('?')[:10]
 
     [UserItem.objects.create(item=item, user=profile) for item in items]
